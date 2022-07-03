9307cf42dc225c8172bb0b9ce09ee08929d9d6dd
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-03 18:22:05 -0500

    made final edits

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -46,7 +46,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-for (designation of stars) {
+for (let designation of stars) {
   console.log(designation);
 }
 
